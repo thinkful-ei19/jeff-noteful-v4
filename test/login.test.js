5c7b84386b92b0583c59057d1d9cc816168ba2f4
Jeff Vega <jeff@intellibliss.com>
2018-04-06 15:08:20 -0700

    yes

--- test/login.test.js
+++ test/login.test.js
@@ -1,115 +1,115 @@
-'use strict';
-const app = require('../server');
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken')
-const {
-  TEST_MONGODB_URI,
-  JWT_SECRET
-} = require('../config');
+// 'use strict';
+// const app = require('../server');
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+// const mongoose = require('mongoose');
+// const jwt = require('jsonwebtoken')
+// const {
+//   TEST_MONGODB_URI,
+//   JWT_SECRET
+// } = require('../config');
 
-const User = require('../models/user')
-const seedUsers = require('../db/seed/users')
+// const User = require('../models/user')
+// const seedUsers = require('../db/seed/users')
 
 
-const expect = chai.expect;
+// const expect = chai.expect;
 
-chai.use(chaiHttp);
+// chai.use(chaiHttp);
 
-describe('Noteful API - Login', function () {
-  // let token;
-  // const fullname = 'Example User';
-  // const username = 'exampleUser';
-  // const password = 'examplePass';
+// describe('Noteful API - Login', function () {
+//   // let token;
+//   // const fullname = 'Example User';
+//   // const username = 'exampleUser';
+//   // const password = 'examplePass';
 
-  before(function () {
-    return mongoose.connect(TEST_MONGODB_URI)
-      .then(() => mongoose.connection.db.dropDatabase());
-  });
+//   before(function () {
+//     return mongoose.connect(TEST_MONGODB_URI)
+//       .then(() => mongoose.connection.db.dropDatabase());
+//   });
 
-  beforeEach(function () {
-    return User.insertMany(seedUsers)
-  });
+//   beforeEach(function () {
+//     return User.insertMany(seedUsers)
+//   });
 
-  afterEach(function () {
-    return mongoose.connection.db.dropDatabase();
-  });
+//   afterEach(function () {
+//     return mongoose.connection.db.dropDatabase();
+//   });
 
-  after(function () {
-    return mongoose.disconnect();
-  });
+//   after(function () {
+//     return mongoose.disconnect();
+//   });
 
-  describe('Noteful /api/login', function () {
+//   describe('Noteful /api/login', function () {
 
-    it('Sould return a valid auth token', function () {
-      return chai.request(app)
-        .post('/api/login')
-        .send({
-          username: 'user0',
-          password: 'password'
-        })
-        .then(res => {
-          expect(res).to.have.status(200);
-          expect(res.body).to.be.an('object');
-          expect(res.body.authToken).to.be.a('string');
-          const token = res.body.authToken;
-          const payload = jwt.verify(res.body.authToken, JWT_SECRET);
+//     it('Sould return a valid auth token', function () {
+//       return chai.request(app)
+//         .post('/api/login')
+//         .send({
+//           username: 'user0',
+//           password: 'password'
+//         })
+//         .then(res => {
+//           expect(res).to.have.status(200);
+//           expect(res.body).to.be.an('object');
+//           expect(res.body.authToken).to.be.a('string');
+//           const token = res.body.authToken;
+//           const payload = jwt.verify(res.body.authToken, JWT_SECRET);
 
-          expect(payload.user).to.not.have.property('password');
-          expect(payload.user).to.deep.equal({
-            "id": "333333333333333333333300",
-            "fullname": "User Zero",
-            "username": "user0"
+//           expect(payload.user).to.not.have.property('password');
+//           expect(payload.user).to.deep.equal({
+//             "id": "333333333333333333333300",
+//             "fullname": "User Zero",
+//             "username": "user0"
 
-          });
-        });
-    });
-    it('Should reject requests with no credentials',function(){
-      return chai
-      .request(app)
-      .post('/api/login')
-      .then(()=>
-    expect.fail(null,null, 'Request should not succeed')
-  )
-  .catch(err => {
-    if(err instanceof chai.AssertionError){
-      throw err;
-    }
-  })
-  const res = err.response;
-  expect(res).to.have.status(400)
-    });
-    it('Should reject requests with incorrect usernames',function(){
-      return chai
-      .request(app)
-      .post('/api/login')
-      .send({username:'WrongUsername',password:'password'})
-      .then(()=>
-      expect.fail(null,null, 'Request should not succeed')
-    )
-    .catch(err => {
-      if(err instanceof chai.AssertionError){
-        throw err;
-      }
-      const res = err.response;
-      expect(res).to.have.status(401)
-    })
-    })
-    it('Should reject requests with incorrect passwords',function(){
-      return chai
-      .request(app)
-      .post('/api/login')
-      .send({username:'user0',password:'password 1=1'})
-      .then(()=>
-    expect.fail(null,null, 'Request should not succeed'))
-    .catch(err => {
-      if(err instanceof chai.AssertionError){
-        throw err;
-      }
-      const res = err.response;
-      expect(res).to.have.status(401)
-    })
-    });
-  });
-});
+//           });
+//         });
+//     });
+//     it('Should reject requests with no credentials',function(){
+//       return chai
+//       .request(app)
+//       .post('/api/login')
+//       .then(()=>
+//     expect.fail(null,null, 'Request should not succeed')
+//   )
+//   .catch(err => {
+//     if(err instanceof chai.AssertionError){
+//       throw err;
+//     }
+//   })
+//   const res = err.response;
+//   expect(res).to.have.status(400)
+//     });
+//     it('Should reject requests with incorrect usernames',function(){
+//       return chai
+//       .request(app)
+//       .post('/api/login')
+//       .send({username:'WrongUsername',password:'password'})
+//       .then(()=>
+//       expect.fail(null,null, 'Request should not succeed')
+//     )
+//     .catch(err => {
+//       if(err instanceof chai.AssertionError){
+//         throw err;
+//       }
+//       const res = err.response;
+//       expect(res).to.have.status(401)
+//     })
+//     })
+//     it('Should reject requests with incorrect passwords',function(){
+//       return chai
+//       .request(app)
+//       .post('/api/login')
+//       .send({username:'user0',password:'password 1=1'})
+//       .then(()=>
+//     expect.fail(null,null, 'Request should not succeed'))
+//     .catch(err => {
+//       if(err instanceof chai.AssertionError){
+//         throw err;
+//       }
+//       const res = err.response;
+//       expect(res).to.have.status(401)
+//     })
+//     });
+//   });
+// });
--- test/server.test.js
+++ test/server.test.js
@@ -1,136 +1,136 @@
-'use strict';
-const app = require('../server');
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+// 'use strict';
+// const app = require('../server');
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
 
-const expect = chai.expect;
+// const expect = chai.expect;
 
-chai.use(chaiHttp);
+// chai.use(chaiHttp);
 
-describe('Server Test', function (){
-  let user;
-  let token;
+// describe('Server Test', function (){
+//   let user;
+//   let token;
 
-  before(function () {
-    return mongoose.connect(TEST_MONGODB_URI)
-      .then(() => mongoose.connection.db.dropDatabase());
-  });
+//   before(function () {
+//     return mongoose.connect(TEST_MONGODB_URI)
+//       .then(() => mongoose.connection.db.dropDatabase());
+//   });
 
 
-  beforeEach(function () {
-    return Promise.all([
-      User.insertMany(seedUsers),
-      Folder.insertMany(seedFolders),
-      Folder.ensureIndexes()
-    ]).then(([users]) => {
+//   beforeEach(function () {
+//     return Promise.all([
+//       User.insertMany(seedUsers),
+//       Folder.insertMany(seedFolders),
+//       Folder.ensureIndexes()
+//     ]).then(([users]) => {
 
-      user = users[0];
-      token = jwt.sign({
-        user
-      }, JWT_SECRET, {
-        subject: user.username
-      })
-    });
-  });
+//       user = users[0];
+//       token = jwt.sign({
+//         user
+//       }, JWT_SECRET, {
+//         subject: user.username
+//       })
+//     });
+//   });
 
-  afterEach(function () {
-    return mongoose.connection.db.dropDatabase();
-  });
+//   afterEach(function () {
+//     return mongoose.connection.db.dropDatabase();
+//   });
 
-  after(function () {
-    return mongoose.disconnect();
-  });
+//   after(function () {
+//     return mongoose.disconnect();
+//   });
 
-  describe('GET /api/folders', function () {
+//   describe('GET /api/folders', function () {
 
-    it('should return the correct number of folders', function () {
-      const dbPromise = Folder.find({
-        userId: user.id
-      });
-      const apiPromise = chai.request(app)
-        .get('/api/folders')
-        .set('Authorization', `Bearer ${token}`);
-      return Promise.all([dbPromise, apiPromise])
-        .then(([data, res]) => {
-          expect(res).to.have.status(200);
-          expect(res).to.be.json;
-          expect(res.body).to.be.a('array');
-          expect(res.body).to.have.length(data.length);
-        });
-    });
+//     it('should return the correct number of folders', function () {
+//       const dbPromise = Folder.find({
+//         userId: user.id
+//       });
+//       const apiPromise = chai.request(app)
+//         .get('/api/folders')
+//         .set('Authorization', `Bearer ${token}`);
+//       return Promise.all([dbPromise, apiPromise])
+//         .then(([data, res]) => {
+//           expect(res).to.have.status(200);
+//           expect(res).to.be.json;
+//           expect(res.body).to.be.a('array');
+//           expect(res.body).to.have.length(data.length);
+//         });
+//     });
 
-    it('should return a list with the correct right fields', function () {
-      const dbPromise = Folder.find({
-        userId: user.id
-      });
-      const apiPromise = chai
-        .request(app)
-        .get('/api/folders')
-        .set('Authorization', ` Bearer ${token}`)
+//     it('should return a list with the correct right fields', function () {
+//       const dbPromise = Folder.find({
+//         userId: user.id
+//       });
+//       const apiPromise = chai
+//         .request(app)
+//         .get('/api/folders')
+//         .set('Authorization', ` Bearer ${token}`)
 
-      return Promise.all([dbPromise, apiPromise])
-        .then(([data, res]) => {
-          expect(res).to.have.status(200);
-          expect(res).to.be.json;
-          expect(res.body).to.be.a('array');
-          expect(res.body).to.have.length(data.length);
-          res.body.forEach(function (item) {
-            expect(item).to.be.a('object');
-            expect(item).to.have.keys('id', 'name', 'userId');
-          });
-        });
-    });
+//       return Promise.all([dbPromise, apiPromise])
+//         .then(([data, res]) => {
+//           expect(res).to.have.status(200);
+//           expect(res).to.be.json;
+//           expect(res.body).to.be.a('array');
+//           expect(res.body).to.have.length(data.length);
+//           res.body.forEach(function (item) {
+//             expect(item).to.be.a('object');
+//             expect(item).to.have.keys('id', 'name', 'userId');
+//           });
+//         });
+//     });
 
-  });
+//   });
 
-describe('Reality Check', () => {
+// describe('Reality Check', () => {
 
-  it('true should be true', () => {
-    expect(true).to.be.true;
-  });
+//   it('true should be true', () => {
+//     expect(true).to.be.true;
+//   });
 
-  it('2 + 2 should equal 4 (except in 1984)', () => {
-    expect(2 + 2).to.equal(4);
-  });
+//   it('2 + 2 should equal 4 (except in 1984)', () => {
+//     expect(2 + 2).to.equal(4);
+//   });
 
-});
+// });
 
-describe('Environment', () => {
+// describe('Environment', () => {
 
-  it('NODE_ENV should be "test"', () => {
-    expect(process.env.NODE_ENV).to.equal('test');
-  });
+//   it('NODE_ENV should be "test"', () => {
+//     expect(process.env.NODE_ENV).to.equal('test');
+//   });
 
-});
+// });
 
-describe('Basic Express setup', () => {
+// describe('Basic Express setup', () => {
 
-  describe('Express static', () => {
+//   describe('Express static', () => {
 
-    it('GET request "/" should return the index page', () => {
-      return chai.request(app)
-        .get('/')
-        .then(function (res) {
-          expect(res).to.exist;
-          expect(res).to.have.status(200);
-          expect(res).to.be.html;
-        });
-    });
+//     it('GET request "/" should return the index page', () => {
+//       return chai.request(app)
+//         .get('/')
+//         .then(function (res) {
+//           expect(res).to.exist;
+//           expect(res).to.have.status(200);
+//           expect(res).to.be.html;
+//         });
+//     });
 
-  });
+//   });
 
-  describe('404 handler', () => {
+//   describe('404 handler', () => {
 
-    it('should respond with 404 when given a bad path', () => {
-      return chai.request(app)
-        .get('/bad/path')
-        .set('Authorization',`Bearer${token}`)
-        .catch(err => err.response)
-        .then(res => {
-          expect(res).to.have.status(404);
-        });
-    });
+//     it('should respond with 404 when given a bad path', () => {
+//       return chai.request(app)
+//         .get('/bad/path')
+//         .set('Authorization',`Bearer${token}`)
+//         .catch(err => err.response)
+//         .then(res => {
+//           expect(res).to.have.status(404);
+//         });
+//     });
 
-  });
-})
-});
+//   });
+// })
+// });
